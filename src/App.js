7351acc0c54627a71851e24dd812673036d33917
John Ngo <john.ngo@MacBook-Air.local>
2024-03-02 13:05:06 -0600

    Add project to github repo, finished tasks from Monday

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './navBar';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Profile' element={<Profile />} />
+        </Routes>
+      </div>
+    </>
   );
 }
-
 export default App;
