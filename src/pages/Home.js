2e4ba1bd0c03e91ec1cf57e5e53f1af7fb3e5737
John Ngo <johnmelrenzo@gmail.com>
2024-03-08 18:38:30 -0600

    added grid layout to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <h1>hello world!</h1>;
-}
+import 'primeflex/primeflex.css';
+
+const gridLayout = () => {
+  return (
+    <div class='grid'>
+      <div class='col'>
+        <div class='text-center p-3 border-round-sm bg-primary font-bold'>
+          card placeholder 1
+        </div>
+      </div>
+      <div class='col'>
+        <div class='text-center p-3 border-round-sm bg-primary font-bold '>
+          card placeholder 2
+        </div>
+      </div>
+      <div class='col'>
+        <div class='text-center p-3 border-round-sm bg-primary font-bold '>
+          card placeholder 3
+        </div>
+      </div>
+      <div class='col'>
+        <div class='text-center p-3 border-round-sm bg-primary font-bold '>
+          card placeholder 4
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default gridLayout;
